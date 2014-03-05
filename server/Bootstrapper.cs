dbc4bc4ef09057cc234ce29266ae0cf60a62f46b
925coder <925coder@gmail.com>
2014-03-05 07:25:58 +0000

    bootstrap added2

---
+++ server/Bootstrapper.cs
@@ -0,0 +1,26 @@
+﻿using Nancy;
+using Nancy.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server
+{
+  public class Bootstrapper : DefaultNancyBootstrapper
+  {
+    protected override Nancy.Diagnostics.DiagnosticsConfiguration DiagnosticsConfiguration
+    {
+      get
+      {
+        return new DiagnosticsConfiguration { Password="abcd"};
+      }
+    }
+
+    protected override void ApplicationStartup(Nancy.TinyIoc.TinyIoCContainer container, Nancy.Bootstrapper.IPipelines pipelines)
+    {
+      StaticConfiguration.EnableRequestTracing = true;
+    }
+  }
+}
